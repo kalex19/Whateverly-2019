1b4ff11de598ea3e75b7e645b85f3d70eca1ec64
Emily Dittmer <emilyedittmer@gmail.com>
2019-04-04 14:24:48 -0600

    Start start-js event-listeners
    Co-Authored-by: Katie Lewis <kale8432@colorado.edu>
    Co-Authored-by: Bridgett Coyle <bacoyle0409@gmail.com>

--- src/Start.js
+++ src/Start.js
@@ -4,11 +4,53 @@ import App from './App.js';
 class Start extends App {
   constructor() {
     super();
+      this.state= {
+        startInputValue: ''
+        //may need state for selected hiking/camping
+      }
 
+    //Method:
+    //Toggle Search
+
+    //Method:
+    //Display Main Page
+
+    //Method:
+    //Display Cards
+    selectCamping = (event) => {
+      event.preventDefault();
+      this.props.Dataset.campground
+    },
+
+    selectHiking = (event) => {
+      event.preventDefault();
+      this.props.Dataset.trail
+    },
+
+    grabLocation = (event) => {
+      this.setState({
+        startInputValue: event.target.value
+      })
+    },
+
+    startSubmitLocation = (event) => {
+      event.preventDefault();
+      this.props.setLocation(this.setState.startInputValue);
+    }
+    ///This will be referencing App Location
   }
+
   render {
     return (
-      
+      <React.Fragment>
+        <input type="button" className="campStartBtn" value="Camping" onClick={selectCamping} />
+        <input type="button" className="hikeStartBtn" value="Hiking" onClick={selectHiking} />
+        <form onSubmit={this.startSubmitLocation}>
+          <label>Search:</label>
+          <input type="text" className="startLocationInput" placeholder="Please choose a Location" value= {this.state.startInputValue} onChange={this.grabLocation} />
+          <input type="submit" className="startSubmitBtn" value="Submit" />
+        </form>
+      </React.Fragment>
     )
   }
 }
