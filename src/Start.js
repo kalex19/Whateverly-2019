da2f501d8e56ccd085347fe497a4b58a9286c6f3
Katherine Lewis <kale8432@colorado.edu>
2019-04-04 15:26:45 -0600

    Styling Start Class
    co-authored-by: Emily Dittmer <emilyedittmer@gmail.com>
    co-authored-by: Bridgett Coyle <bacoyle0409@gmail.com>

--- src/Start.js
+++ src/Start.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
 import App from './App.js';
+import './Start.css';
 
 class Start extends App {
   constructor() {
     super();
       this.state= {
-        startInputValue: ''
+        startInputValue: '',
+        shown: true
         //may need state for selected hiking/camping
       }
 
-    //Method:
-    //Toggle Search
+    displayMainPage = () => {
+      this.setState({
+        shown: !this.state.shown
+      })
+    }
 
-    //Method:
-    //Display Main Page
-
-    //Method:
-    //Display Cards
     selectCamping = (event) => {
       event.preventDefault();
       this.props.Dataset.campground
@@ -41,11 +41,21 @@ class Start extends App {
   }
 
   render {
+
+    let shown = {
+      display: this.state.shown ? visible : hidden
+    };
+
+    let hidden = {
+      display: this.state.shown ? hidden : visible
+    };
+
     return (
-      <React.Fragment>
-        <input type="button" className="campStartBtn" value="Camping" onClick={selectCamping} />
-        <input type="button" className="hikeStartBtn" value="Hiking" onClick={selectHiking} />
-        <form onSubmit={this.startSubmitLocation}>
+      <React.Fragment className="startWrapper" style={shown}>
+        <input type="button" className="campStartBtn visibility"  value="Camping" onClick={selectCamping} />
+        //onClick={this.toggle.bind(this)}
+        <input type="button" className="hikeStartBtn visibility" value="Hiking" onClick={selectHiking} />
+        <form onSubmit={this.startSubmitLocation} className=' startLocationSearch searchVisibility' style={ hidden } >
           <label>Search:</label>
           <input type="text" className="startLocationInput" placeholder="Please choose a Location" value= {this.state.startInputValue} onChange={this.grabLocation} />
           <input type="submit" className="startSubmitBtn" value="Submit" />
