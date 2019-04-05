2c169853c06f4cff521076a818fc2617fa619b1c
Bridgett Coyle <bacoyle0409@gmail.com>
2019-04-05 11:13:53 -0600

    Change location of dataset, add componentdidmount
    Co-Authored-By: Emily Dittmer <emilyedittmer@gmail.com>
    Co-Authored-By: Katie Lewis <kale8432@colorado.edu>

--- src/App/App.js
+++ src/App/App.js
@@ -1,37 +1,18 @@
 import React, {Component} from 'react';
-import {camping, trails} from './Data.js';
+// import {camping, trails} from '../Data/Data.js';
 import './App.css';
-import Start from './Start.js';
+import Start from '../Start/Start.js';
 // import Card from './Card.js';
 
-const Dataset = camping.reduce((accum, campground) => {
-  if(!accum[campground.location]) {
-    let newData = accum[campground.location];
-    let currentCampground = campground.location;
-
-  let availCampgrounds = camping.filter(campground => {
-      if(currentCampground === campground.location) {
-          return campground;
-      }
-  });
-  let availTrails = trails.filter(trail => {
-      if(campground.location === trail.location) {
-          return trail
-      }
-  });
-
-  newData = {
-      'campground': availCampgrounds,
-      'trail': availTrails
-      }
-  }
-return accum
-}, {});
 
 export default class App extends Component {
   constructor() {
     super();
 
+    this.state ={ 
+      camping:[],
+      trail: []
+    }
     //const data =  passing in datafiles (data object)
     //declare state
     //can we use two states? Or need to combo dataset?
@@ -46,6 +27,56 @@ export default class App extends Component {
     //toggle css hidden class/z-index
 
   }
+  componentDidMount() {
+    fetch('https://fe-apps.herokuapp.com/api/v1/whateverly/1901/b-coyle/camping')
+      .then(response => response.json()) 
+      .then(selectCamping => {
+          this.setState({			    
+            camping: this.cleanUpData(selectCamping) 
+        });
+      })
+      .catch(error => console.log('Camping Error', error));
+  
+      fetch('https://fe-apps.herokuapp.com/api/v1/whateverly/1901/b-coyle/trails')
+        .then(response => response.json()) 
+        .then(selectHiking => {
+          this.setState({			
+            trail: this.cleanUpData(selectHiking) 
+        });	
+      })
+        .catch(error => console.log('Hiking Error', error)); 	 
+   }
+
+   cleanUpData(campgrounds, trails) {
+    let newData;
+    const Dataset = campgrounds.reduce((accum, campground) => {
+      if(!accum[campground.location]) {
+        newData = accum[campground.location];
+        console.log('Test newData', newData)
+        let currentCampground = campground.location;
+    
+      let availCampgrounds = campgrounds.filter(campground => {
+          if(currentCampground === campground.location) {
+              return campground;
+          }
+      });
+      let availTrails = trails.filter(trail => {
+          if(campground.location === trail.location) {
+              return trail;
+          }
+      });
+    
+      newData = {
+          'campground': availCampgrounds,
+          'trail': availTrails
+          }
+      }
+    return accum
+    }, {});
+    console.log('Test Dataset', Dataset);
+    return newData
+   }
+
   render() {
     return (
       <div className='startScreen'>
