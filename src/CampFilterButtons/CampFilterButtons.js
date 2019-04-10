be5a9b2943bba436a8bd0ed2f0ce0e81efe48a0c
Emily Dittmer <emilyedittmer@gmail.com>
2019-04-09 21:52:44 -0600

    Filters through camp on click
    Co-Authored-by: Bridgett Coyle <bacoyle0409@gmail.com>
    Co-Authored-by: Katie Lewis <kale8432@colorado.edu>

--- src/CampFilterButtons/CampFilterButtons.js
+++ src/CampFilterButtons/CampFilterButtons.js
@@ -5,47 +5,63 @@ import Styles from './CampFilterButtons.scss';
 export default class CampFilterButtons extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      updateFilter: []
+    }
   }
 
   allowsTents = (props) => {
     let tentCamp = this.props.filteredCampgrounds.filter(campground => campground.camping.includes("Tent"))
+    this.state.updateFilter.push(tentCamp);
     return tentCamp;
   }
 
   allowsCars = (props) => {
     let carCamp = this.props.filteredCampgrounds.filter(campground => campground.camping.includes("Car"))
+    this.state.updateFilter.push(carCamp);
     return carCamp;
   }
 
   allowsRVs = (props) => {
     let rvCamp = this.props.filteredCampgrounds.filter(campground => campground.camping.includes("RV"))
+    this.state.updateFilter.push(rvCamp);
     return rvCamp;
   }
 
   isLess5000 = (props) => {
     let less5000Camp = this.props.filteredCampgrounds.filter(campground => campground['elevation'] <= 4999)
+    this.state.updateFilter.push(less5000Camp);
     return less5000Camp;
   }
 
   isLess9000 = (props) => {
     let less9000Camp = this.props.filteredCampgrounds.filter(campground => campground['elevation'] >= 5000 && campground['elevation']<= 8999)
+    this.state.updateFilter.push(less9000Camp);
     return less9000Camp;
   }
 
   isLess11000 = (props) => {
     let less11000Camp = this.props.filteredCampgrounds.filter(campground => campground['elevation'] >= 9000 && campground['elevation']<= 11000)
+    this.state.updateFilter.push(less11000Camp);
     return less11000Camp;
   }
 
   yesRes = (props) => {
     let yes = this.props.filteredCampgrounds.filter(campground => campground['reservation-available'] === true)
+    this.state.updateFilter.push(yes);
+    console.log('yes', this.state.updateFilter);
     return yes;
   }
 
   noRes = (props) => {
     let no = this.props.filteredCampgrounds.filter(campground => campground['reservation-available'] === false)
+    this.state.updateFilter.push(no);
     return no;
+  }
+
+  filter = (event) => {
+    event.preventDefault();
+    console.log('HI', this.state.updateFilter);
   }
 
   render() {
