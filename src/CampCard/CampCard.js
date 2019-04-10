b76702eadcf2132a5abb699e2efd6113006b0509
Emily Dittmer <emilyedittmer@gmail.com>
2019-04-10 17:43:52 -0600

    Add local storage to cards
    Co-Authored-by: Bridgett Coyle <bacoyle0409@gmail.com>
    Co-Authored-by: Katie Lewis <kale8432@colorado.edu>

--- src/CampCard/CampCard.js
+++ src/CampCard/CampCard.js
@@ -8,6 +8,9 @@ export default class CampCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      favorite: false,
+      visited: false,
+      deleted: false,
       popUp: false
     }
   }
@@ -18,6 +21,41 @@ export default class CampCard extends Component {
     console.log(this.state);
   }
 
+  handleDeleteCampCard() {
+    //create another method that will have the logic to delete the card
+    //potentially have on the app and parse down
+    //if this button is clicked remove from localStorage/component that holds it in local storage and remove with that component 
+    this.setState({
+      deleted: true
+    })
+  }
+
+  handleFavoriteCamp = (event) => {
+    //will have to save to component/LocalStorage  and toggle between favorite and not
+    event.preventDefault();
+    this.setState({
+      favorite: !this.state.favorite
+    });
+  }
+
+  handleVisitedCamp = (event) => {
+    //will have to save to a component/Localstorage and toggle between visited and not
+    event.preventDefault();
+    this.setState({
+      visited: !this.state.visited
+    })
+  }
+
+  componentDidUpdate = () => {
+    localStorage.setItem(this.props.filteredCampgrounds.name, JSON.stringify(this.state));
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      favorite: JSON.parse(localStorage.getItem(this.props.filteredCampgrounds.name)) || false,
+      visited: JSON.parse(localStorage.getItem(this.props.filteredCampgrounds.name)) || false
+    })
+  }
   render (props) {
     let campPopUp;
     if(this.state.popUp === true) {
@@ -31,9 +69,9 @@ export default class CampCard extends Component {
       <section className='campingCard'>
         <div>
           <h2 className='campName'>{ this.props.filteredCampgrounds.name }</h2>
-          <button className='favoriteButton'>Favorite</button>
-          <button className='visitedButton'>Visited</button>
-          <button className='deleteButton'>Delete</button>
+          <button className='favoriteButton' onClick={this.handleFavoriteCamp}>  <i className="fas fa-heart"></i></button>
+          <button className='visitedButton' onClick={this.handleVisitedCamp}> <i className="fas fa-check"></i></button>
+          <button className='deleteButton' onClick={this.handleDeleteCampCard}><i className="far fa-trash-alt"></i></button>
         </div>
         <h3><span className='textLabel'>Location: </span>{ this.props.filteredCampgrounds.location }</h3>
         <h3><span className='textLabel'>Available Seasons: </span>{ this.props.filteredCampgrounds.season }</h3>
